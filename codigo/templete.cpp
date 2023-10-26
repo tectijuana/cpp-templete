242b12643c08ca09a47c919bd50d3b5686881115
René Solis <2523851+IoTeacher@users.noreply.github.com>
2023-10-26 14:29:44 -0700

    Create templete.cpp

---
+++ codigo/templete.cpp
@@ -0,0 +1,22 @@
+/*
+ * Nombre del Archivo: main.c
+ * Autor:   [Tu Nombre]
+ * Correo:  [Tu Correo]
+ * Fecha:   [Fecha]
+ * Curso:   Lenguajes de Interfaz, TECNM Campus ITT
+ * 
+ * Objetivo:
+ * Este programa está diseñado para [proporcionar una breve descripción del objetivo del programa].
+ *
+ * Historial de Revisiones:
+ * [Fecha]        [Tu Nombre] - Creado
+ * [Fecha]        [Tu Nombre] - Actualizado para añadir [característica/corrección]
+ *
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    // Tu código aquí
+    return 0;
+}
